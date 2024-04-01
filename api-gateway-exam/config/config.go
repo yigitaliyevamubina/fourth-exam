0cda8f0cd1133e9ca3ec22941f4f0e5eeba26e4a
Mubina <mubinayigitaliyeva00@gmail.com>
2024-04-01 21:55:25 +0500

    updated api

--- api-gateway-exam/config/config.go
+++ api-gateway-exam/config/config.go
@@ -55,22 +55,22 @@ func Load() Config {
 	c.LogLevel = cast.ToString(getOrReturnDefault("LOG_LEVEL", "debug"))
 	c.HTTPPort = cast.ToString(getOrReturnDefault("HTTP_PORT", ":4040"))
 
-	c.RedisHost = cast.ToString(getOrReturnDefault("REDIS_HOST", "localhost"))
+	c.RedisHost = cast.ToString(getOrReturnDefault("REDIS_HOST", "redisdb"))
 	c.RedisPort = cast.ToInt(getOrReturnDefault("REDIS_PORT", 6379))
 
-	c.PostgresHost = cast.ToString(getOrReturnDefault("POSTGRES_HOST", "localhost"))
+	c.PostgresHost = cast.ToString(getOrReturnDefault("POSTGRES_HOST", "db"))
 	c.PostgresPort = cast.ToInt(getOrReturnDefault("POSTGRES_PORT", 5432))
 	c.PostgresDatabase = cast.ToString(getOrReturnDefault("POSTGRES_DATABASE", "socialdb"))
 	c.PostgresUser = cast.ToString(getOrReturnDefault("POSTGRES_USER", "postgres"))
 	c.PostgresPassword = cast.ToString(getOrReturnDefault("POSTGRES_PASSWORD", "mubina2007"))
 
-	c.UserServiceHost = cast.ToString(getOrReturnDefault("USER_SERVICE_HOST", "localhost"))
+	c.UserServiceHost = cast.ToString(getOrReturnDefault("USER_SERVICE_HOST", "userservice"))
 	c.UserServicePort = cast.ToInt(getOrReturnDefault("USER_SERVICE_PORT", 9090))
 
-	c.PostServiceHost = cast.ToString(getOrReturnDefault("POST_SERVICE_HOST", "localhost"))
+	c.PostServiceHost = cast.ToString(getOrReturnDefault("POST_SERVICE_HOST", "postservice"))
 	c.PostServicePort = cast.ToInt(getOrReturnDefault("POST_SERVICE_PORT", 7070))
 
-	c.CommentServiceHost = cast.ToString(getOrReturnDefault("COMMENT_SERVICE_HOST", "localhost"))
+	c.CommentServiceHost = cast.ToString(getOrReturnDefault("COMMENT_SERVICE_HOST", "commentservice"))
 	c.CommentServicePort = cast.ToInt(getOrReturnDefault("COMMENT_SERVICE_PORT", 8080))
 
 	c.AccessTokenTimeout = cast.ToInt(getOrReturnDefault("ACCESS_TOKEN_TIMEOUT", 1000))
@@ -79,7 +79,7 @@ func Load() Config {
 	c.AuthConfigPath = cast.ToString(getOrReturnDefault("AUTH_CONFIG_PATH", "./config/auth.conf"))
 	c.AuthCSVPath = cast.ToString(getOrReturnDefault("AUTH_CSV_PATH", "./config/auth.csv"))
 	//Mongo
-	c.MongoDBhost = cast.ToString(getOrReturnDefault("MONGODB_HOST", "localhost"))
+	c.MongoDBhost = cast.ToString(getOrReturnDefault("MONGODB_HOST", "mongodb"))
 	c.MongoDBport = cast.ToString(getOrReturnDefault("MONGODB_PORT", 27017))
 	c.MongoDBdatabase = cast.ToString(getOrReturnDefault("MONGODB_DATABASE", "socialdb"))
 	c.MongoDBCollection = cast.ToString(getOrReturnDefault("MONGODB_COLLECTION", "admins"))
